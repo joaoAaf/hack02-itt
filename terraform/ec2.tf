4e29062b195dd0c2a4f4c6343f1ba4e39a8ca387
joaoAaf <joao.anderson.af@gmail.com>
2023-12-10 20:15:55 -0300

    teste

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -7,11 +7,11 @@ resource "aws_instance" "hack02-itt" {
     Name = "hack02-itt"
   }
   provisioner "file" {
-    source = "./terraform/configs.sh"
+    source = "configs.sh"
     destination = "configs.sh"
   }
   provisioner "file" {
-    source = "./terraform/docker-compose.yml"
+    source = "docker-compose.yml"
     destination = "docker-compose.yml"
   }
   provisioner "remote-exec" {
