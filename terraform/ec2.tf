6edba19770e15f7e66347c5bd51585ec7a5c5080
joaoAaf <joao.anderson.af@gmail.com>
2023-12-11 15:14:41 -0300

    alteração nome

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "hack02-itt" {
   key_name               = "itt-keys"
   vpc_security_group_ids = ["sg-051e34146616a7798", "sg-0ab4589c664938387", "sg-01033e2f069124087"]
   tags = {
-    Name = "hack02-itt"
+    Name = "hack02-itt-2"
   }
   user_data = file("${path.module}/configs.sh")
 }
